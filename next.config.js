399ee75cc7e72561cba579c349fbdd5a02377a6d
Godwin Daniel <lemuelgodwin2@gmail.com>
2025-09-04 23:28:58 +0100

    refactor: update next.config.js to use ES module syntax and improve tsconfig.json formatting

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        pathname: '/**',
-      },
-    ],
-    dangerouslyAllowSVG: true, // Enable SVG images
-  },
+export const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'placehold.co',
+      pathname: '/**',
+    },
+  ],
+  dangerouslyAllowSVG: true, // Enable SVG images
 };
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: 'build', // change .next to build
+}
+
+export default nextConfig
